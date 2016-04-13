3d1c755a307848908adbd6b159361a58af60c845
Zhicong Huang <zhihuang@icsil1noteb176.epfl.ch>
2016-04-13 16:41:02 +0200

    change jvm memory size

--- run/run_garblers.sh
+++ run/run_garblers.sh
@@ -1,4 +1,4 @@
 for i in $(seq 0 $(($1 - 1)))
 do
-  java -Xmx3700m -cp ../bin/:../lib/* graphsc.parallel.Machine -garblerId $i -garblerPort $((35000 + $i )) -isGen true -inputLength $2 -program $3 -totalGarblers $1 -machineConfigFile $4 -mode $5 -peerBasePort 50000 -offline $6 &
+  java -Xmx1536m -cp ../bin/:../lib/* graphsc.parallel.Machine -garblerId $i -garblerPort $((35000 + $i )) -isGen true -inputLength $2 -program $3 -totalGarblers $1 -machineConfigFile $4 -mode $5 -peerBasePort 50000 -offline $6 &
 done
