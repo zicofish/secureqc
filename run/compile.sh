405ca471c0b986e8021eb3eb17eea6a15bbdca08
Zhicong Huang <zhihuang@icsil1noteb176.epfl.ch>
2016-04-13 16:49:11 +0200

    change compile path

--- run/compile.sh
+++ run/compile.sh
@@ -1,4 +1,4 @@
 find ../ -name "*.java" > source.txt;
 #javac -cp .:../lib/FlexSC.jar:../lib/commons-cli-1.2.jar:../lib/commons-io-2.4.jar -d . @source.txt;
-javac -cp .:../lib/* -d . @source.txt;
+javac -cp ../bin/:../lib/* -d ../bin/ @source.txt;
 rm source.txt;
