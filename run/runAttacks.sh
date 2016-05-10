4eb4bd715d0f7dd5eb6192e855aa8235c70ffca0
Zhicong Huang <zhihuang@icsil1noteb176.epfl.ch>
2016-05-10 16:35:10 +0200

    clear

--- run/runAttacks.sh
+++ run/runAttacks.sh
@@ -3,7 +3,7 @@
 #numOfSNPs=(10 100 200)
 #suffix=("dp0.01")
 numOfSNPs=(1000 5000 10000 50000 100000 500000 1000000)
-suffix=("" "dp1" "dp0.5" "dp0.1" "dp0.05" "dp0.01")
+suffix=("clear" "dp1" "dp0.5" "dp0.1" "dp0.05" "dp0.01")
 
 cd ../script/attacks
 for ((i=0; i<=`expr ${#numOfSNPs[@]} - 1`; i++))
