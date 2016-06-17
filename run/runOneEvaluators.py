13e767c31ed4304cd7f897c30efb3d6561a5f93f
Zhicong Huang <zhihuang@icsil1noteb176.epfl.ch>
2016-06-17 11:17:15 +0200

    split garlbers and evaluators

---
+++ run/runOneEvaluators.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import subprocess
+import os.path
+import time
+import sys
+inputLength = sys.argv[2]
+garblers = sys.argv[3]
+experiment = sys.argv[1]
+input_spec = sys.argv[4]
+subprocess.call("./clear_ports.sh", shell=True)
+params = str(garblers) + " " + str(inputLength) + " " + experiment + " 00 REAL false " + input_spec
+subprocess.call(["./run_evaluators.sh " + params], shell=True)
